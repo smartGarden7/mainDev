6d0fea4f9b19136ea3a2f9e0ba9ade574a053829
smartGarden7 <smartgarden7@gmail.com>
2016-09-06 23:16:23 +0300

    update main ino according to Flow Chart algorithm

--- smartGarden/smartGarden.ino
+++ smartGarden/smartGarden.ino
@@ -7,10 +7,31 @@ void setup() {
 	init_display();
 	Serial.begin(115200);
 	connectToWiFi();
-	main_screen();
+	main_screen();	// should contain the info about WIFi condition and be ready for next functions
 }
 
 void loop() {
-  askForManualProg();
-  autoProgram();
+	if(WiFi == false){
+		if(irrigationCompleted == true) {
+			delay48hours();
+			irrigationCompleted == false;
+		}
+		else {
+			if(askForManualProg() == true) 	{
+				manualProg();	// irrigate 3 hours
+			}
+			else {
+				connectToWiFi();
+				autoProgram(WiFi);	// false for program without WiFi, true for prgram with WiFi.
+			}
+		}
+	}
+	else {
+		if(askForManualProg == false) {
+			autoProgram(WiFi);
+		}
+		else {
+			manualProg();	// irrigate 3 hours
+		}
+	}
 }
