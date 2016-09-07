f91a9a5cd5a27fea19543ec27850b62c4a5ed041
smartGarden7 <smartgarden7@gmail.com>
2016-09-08 00:11:11 +0300

    added tasks

--- smartGarden/smartGarden.ino
+++ smartGarden/smartGarden.ino
@@ -2,6 +2,19 @@
 #include "tft.hpp"
 #include "global.hpp"
 
+/* Tasks before 09/09/16:
+	1. Change this file according to lastest flow chart.
+	2. Upgrade functions to meet flow chart requirements.
+	3. Add EEPROM support in a case of unexpected power off:
+		- After each 10min of irrigation update dedicated EEPROM register according to irrigation progress.
+		- On startup, in void loop first check EEPROM: EEPROM.read(address) to cover all possible incidents:
+			* Power off in middle of irrigation process - calculate how much time left and continue.
+			* Irrigation failure occured - wait for user to fix problem and update EEPROM register.
+	4. Check graphical functions and user experience.
+	5. Compile whole smartGarden code and upload to Arduino.
+	6*. Work on wunderground library to get precipitation and make function!
+*/
+
 void setup() {
 	initPorts();
 	init_display();
