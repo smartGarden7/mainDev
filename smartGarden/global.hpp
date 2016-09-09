b015c36f115c1ccfa34fa29d5f4d16cc567ace0c
smartGarden7 <smartgarden7@gmail.com>
2016-09-10 01:02:13 +0300

    added bool irrigationCompleted, program indicating varibles removed

--- smartGarden/global.hpp
+++ smartGarden/global.hpp
@@ -14,21 +14,17 @@ extern const int statusLED;           // D10 - output - GREEN - stable green whe
 extern const int irrigation;          // D11 - output - triggers the relay to open the valve
 extern const int manualStart_button;  // D13 - input - connected to push button which triggers manual program
 
-extern const int manualProgram;       // Manual irrigation integer = 1;
-extern const int autoProgram;         // Automatic irrigation integer = 0;
-
                                       // Variables:
-extern bool wiFi;         
+extern bool wiFi;                     // WiFi condition
+extern bool irrigationCompleted;      // changes to 'true' after 3 hrs of irrigation
 extern int moistSensor_1_val;         // Moisture value of sensor 1
 extern int moistSensor_2_val;         // Moisture value of sensor 2
 extern int moistSensor_3_val;         // Moisture value of sensor 3
+
 extern String receivedString;
 extern String date;
 extern String currentTime;
 extern String currentTemp;
 extern String rainChanceToday_string;
 extern String rainChanceTomorrow_string;
-//extern int avrMoist;                // Average moisture
-
-
 #endif
