79196c198e49e1b57f3b6e689b9d3e6e9c27c41e
smartGarden7 <smartgarden7@gmail.com>
2016-09-09 18:16:33 +0300

    Update main.hpp

--- smartGarden/main.hpp
+++ smartGarden/main.hpp
@@ -5,7 +5,7 @@ extern void initPorts(void);                                      // Initialize 
 extern void checkFailures(byte inProcess, byte irrigationFail);   // use EEPROM registers to determine faliures
 extern void connectToWiFi(void);                                  // Wait for ESP8266 to send data over serial (RX)
 
-extern void delay48hours(void);                                   // This delay function should include message to diplay
+extern void delayHours(int hours);                                // This delay function should include message to diplay
 extern bool askForManualProg(void);                               // waits for user input for 1 minute
 extern void manualProg(void);                                     // irrigate for 3 hours, message to display
 extern void autoProg(bool wifi);                                  // Automatic program - depends on WiFi existence
