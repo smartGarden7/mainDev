7c5c8aa8954e1c24fed35512c3875ab5500e0a18
smartGarden7 <smartgarden7@gmail.com>
2016-09-12 11:30:07 +0300

    Update main.cpp

--- smartGarden/main.cpp
+++ smartGarden/main.cpp
@@ -17,7 +17,7 @@ void initPorts(void) {
   digitalWrite(activateSensors_pin, LOW);
 }
 
-void checkFailures(byte minutesPassed, byte irrigationFail) {
+void checkFailures(int minutesPassed, int irrigationFail) {
 
 }
 
