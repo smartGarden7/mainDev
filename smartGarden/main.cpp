dde851adaf773819075e23b1ec05d5010455d618
smartGarden7 <smartgarden7@gmail.com>
2016-09-12 23:47:01 +0300

    Update main.cpp

--- smartGarden/main.cpp
+++ smartGarden/main.cpp
@@ -17,8 +17,22 @@ void initPorts(void) {
   digitalWrite(activateSensors_pin, LOW);
 }
 
-void checkFailures(int minutesPassed, int irrigationFail) {
-
+extern void checkFailures(int minutesPassed, bool irrigationFail) {
+  print_displa("Starting initialization test");
+  if(minutesPassed != 180) {
+    int minutesLeft = 180 - minutesPassed;
+    print_display("Irrigation Error.",size,color,x,y);
+    printint_display(minutesLeft,size,color,x,y)
+    print_display("minutes left. Re-starting irrigation process.",size,color,x,y)
+    irrigateMinutes(minutesLeft);
+  }
+  else if(irrigationFail == true) {
+    print_display("Something is wrong with the water/valve, fix the problem and click the button.")
+    waitForFix(); // System is blocked until user pushed the button.
+  }
+  else {
+    print_display("Succesfully passed initialization test.")
+  }
 }
 
 void connectToWiFi(void) {     // Waiting for incoming serial data (30 seconds)
