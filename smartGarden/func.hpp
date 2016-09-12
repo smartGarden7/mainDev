da4b42d33fdb3570629a5718f4b886927bed24bb
smartGarden7 <smartgarden7@gmail.com>
2016-09-12 11:37:50 +0300

    Update func.hpp

--- smartGarden/func.hpp
+++ smartGarden/func.hpp
@@ -5,6 +5,7 @@ extern void readMoistureSensors(void);
 extern void readSensor(int sensor);
 extern int  countDrySensors(void);
 extern bool getForecast(void);
-extern void irrigateMinutes(int minutes);
+extern void irrigateMinutes(int minutes); // after each minute of irrigation, increase EEPROM at address #1 
+                                          // At the end of this function print to display "Finished irrigation". 
 extern void blinkLED(int pin, int freq);
 #endif
